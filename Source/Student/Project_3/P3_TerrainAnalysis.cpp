ab5520e90a77d039d8d3834418adbf5dd8071cb9
moskittle <sfmarc1iang@gmail.com>
2021-10-23 10:34:27 -0700

    Draw wrong openness(temp save)

--- Source/Student/Project_3/P3_TerrainAnalysis.cpp
+++ Source/Student/Project_3/P3_TerrainAnalysis.cpp
@@ -9,227 +9,284 @@
 
 bool ProjectThree::implemented_fog_of_war() const // extra credit
 {
-    return false;
+	return false;
 }
 
 float distance_to_closest_wall(int row, int col)
 {
-    /*
-        Check the euclidean distance from the given cell to every other wall cell,
-        with cells outside the map bounds treated as walls, and return the smallest
-        distance.  Make use of the is_valid_grid_position and is_wall member
-        functions in the global terrain to determine if a cell is within map bounds
-        and a wall, respectively.
-    */
+	/*
+		Check the euclidean distance from the given cell to every other wall cell,
+		with cells outside the map bounds treated as walls, and return the smallest
+		distance.  Make use of the is_valid_grid_position and is_wall member
+		functions in the global terrain to determine if a cell is within map bounds
+		and a wall, respectively.
+	*/
 
-    // WRITE YOUR CODE HERE
-    
-    return 0.0f; // REPLACE THIS
+	// WRITE YOUR CODE HERE
+	float distance = 0.0f;
+	float closestDistance = std::numeric_limits<float>::max();
+	int width = terrain->get_map_width(), height = terrain->get_map_height();
+	GridPos currentPos = GridPos(row, col);
+
+	int range = 1;
+	std::vector<GridPos> searchCells;
+	bool isWallFound = false;
+	for (int i = row - range; i <= row + range; ++i)
+	{
+		closestDistance = std::numeric_limits<float>::max();
+		for (int j = col - range; j <= col + range; ++j)
+		{
+			GridPos newNeighborPos = GridPos(i, j);
+			if (newNeighborPos == currentPos) { continue; }
+
+			if (terrain->is_valid_grid_position(newNeighborPos))
+			{
+				if (terrain->is_wall(newNeighborPos))
+				{
+					int diffX = abs(currentPos.col - newNeighborPos.col);
+					int diffY = abs(currentPos.row - newNeighborPos.row);
+					distance = sqrt((float)(diffX * diffX + diffY * diffY));
+					closestDistance = std::min(closestDistance, distance);
+					isWallFound = true;
+				}
+			}
+		}
+
+		if (isWallFound) { break; }
+
+		range++;
+
+		if (range > width || range > height) { break; }
+	}
+
+
+	return closestDistance; // REPLACE THIS
 }
 
 bool is_clear_path(int row0, int col0, int row1, int col1)
 {
-    /*
-        Two cells (row0, col0) and (row1, col1) are visible to each other if a line
-        between their centerpoints doesn't intersect the four boundary lines of every
-        wall cell.  You should puff out the four boundary lines by a very tiny amount
-        so that a diagonal line passing by the corner will intersect it.  Make use of the
-        line_intersect helper function for the intersection test and the is_wall member
-        function in the global terrain to determine if a cell is a wall or not.
-    */
+	/*
+		Two cells (row0, col0) and (row1, col1) are visible to each other if a line
+		between their centerpoints doesn't intersect the four boundary lines of every
+		wall cell.  You should puff out the four boundary lines by a very tiny amount
+		so that a diagonal line passing by the corner will intersect it.  Make use of the
+		line_intersect helper function for the intersection test and the is_wall member
+		function in the global terrain to determine if a cell is a wall or not.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 
-    return false; // REPLACE THIS
+	return false; // REPLACE THIS
 }
 
-void analyze_openness(MapLayer<float> &layer)
+void analyze_openness(MapLayer<float>& layer)
 {
-    /*
-        Mark every cell in the given layer with the value 1 / (d * d),
-        where d is the distance to the closest wall or edge.  Make use of the
-        distance_to_closest_wall helper function.  Walls should not be marked.
-    */
+	/*
+		Mark every cell in the given layer with the value 1 / (d * d),
+		where d is the distance to the closest wall or edge.  Make use of the
+		distance_to_closest_wall helper function.  Walls should not be marked.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
+
+	int width = terrain->get_map_width();
+	int height = terrain->get_map_height();
+
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			GridPos currentPos = GridPos(i, j);
+
+			if (terrain->is_wall(currentPos))
+			{
+				continue;
+			}
+
+			// fill color
+			float distance = distance_to_closest_wall(i, j);
+			float openness = 1.0f / (distance * distance);
+			layer.set_value(i, j, openness);
+		}
+	}
 }
 
-void analyze_visibility(MapLayer<float> &layer)
+void analyze_visibility(MapLayer<float>& layer)
 {
-    /*
-        Mark every cell in the given layer with the number of cells that
-        are visible to it, divided by 160 (a magic number that looks good).  Make sure
-        to cap the value at 1.0 as well.
+	/*
+		Mark every cell in the given layer with the number of cells that
+		are visible to it, divided by 160 (a magic number that looks good).  Make sure
+		to cap the value at 1.0 as well.
 
-        Two cells are visible to each other if a line between their centerpoints doesn't
-        intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
-        helper function.
-    */
+		Two cells are visible to each other if a line between their centerpoints doesn't
+		intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
+		helper function.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void analyze_visible_to_cell(MapLayer<float> &layer, int row, int col)
+void analyze_visible_to_cell(MapLayer<float>& layer, int row, int col)
 {
-    /*
-        For every cell in the given layer mark it with 1.0
-        if it is visible to the given cell, 0.5 if it isn't visible but is next to a visible cell,
-        or 0.0 otherwise.
+	/*
+		For every cell in the given layer mark it with 1.0
+		if it is visible to the given cell, 0.5 if it isn't visible but is next to a visible cell,
+		or 0.0 otherwise.
 
-        Two cells are visible to each other if a line between their centerpoints doesn't
-        intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
-        helper function.
-    */
+		Two cells are visible to each other if a line between their centerpoints doesn't
+		intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
+		helper function.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void analyze_agent_vision(MapLayer<float> &layer, const Agent *agent)
+void analyze_agent_vision(MapLayer<float>& layer, const Agent* agent)
 {
-    /*
-        For every cell in the given layer that is visible to the given agent,
-        mark it as 1.0, otherwise don't change the cell's current value.
+	/*
+		For every cell in the given layer that is visible to the given agent,
+		mark it as 1.0, otherwise don't change the cell's current value.
 
-        You must consider the direction the agent is facing.  All of the agent data is
-        in three dimensions, but to simplify you should operate in two dimensions, the XZ plane.
+		You must consider the direction the agent is facing.  All of the agent data is
+		in three dimensions, but to simplify you should operate in two dimensions, the XZ plane.
 
-        Take the dot product between the view vector and the vector from the agent to the cell,
-        both normalized, and compare the cosines directly instead of taking the arccosine to
-        avoid introducing floating-point inaccuracy (larger cosine means smaller angle).
+		Take the dot product between the view vector and the vector from the agent to the cell,
+		both normalized, and compare the cosines directly instead of taking the arccosine to
+		avoid introducing floating-point inaccuracy (larger cosine means smaller angle).
 
-        Give the agent a field of view slighter larger than 180 degrees.
+		Give the agent a field of view slighter larger than 180 degrees.
 
-        Two cells are visible to each other if a line between their centerpoints doesn't
-        intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
-        helper function.
-    */
+		Two cells are visible to each other if a line between their centerpoints doesn't
+		intersect the four boundary lines of every wall cell.  Make use of the is_clear_path
+		helper function.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void propagate_solo_occupancy(MapLayer<float> &layer, float decay, float growth)
+void propagate_solo_occupancy(MapLayer<float>& layer, float decay, float growth)
 {
-    /*
-        For every cell in the given layer:
+	/*
+		For every cell in the given layer:
 
-            1) Get the value of each neighbor and apply decay factor
-            2) Keep the highest value from step 1
-            3) Linearly interpolate from the cell's current value to the value from step 2
-               with the growing factor as a coefficient.  Make use of the lerp helper function.
-            4) Store the value from step 3 in a temporary layer.
-               A float[40][40] will suffice, no need to dynamically allocate or make a new MapLayer.
+			1) Get the value of each neighbor and apply decay factor
+			2) Keep the highest value from step 1
+			3) Linearly interpolate from the cell's current value to the value from step 2
+			   with the growing factor as a coefficient.  Make use of the lerp helper function.
+			4) Store the value from step 3 in a temporary layer.
+			   A float[40][40] will suffice, no need to dynamically allocate or make a new MapLayer.
 
-        After every cell has been processed into the temporary layer, write the temporary layer into
-        the given layer;
-    */
-    
-    // WRITE YOUR CODE HERE
+		After every cell has been processed into the temporary layer, write the temporary layer into
+		the given layer;
+	*/
+
+	// WRITE YOUR CODE HERE
 }
 
-void propagate_dual_occupancy(MapLayer<float> &layer, float decay, float growth)
+void propagate_dual_occupancy(MapLayer<float>& layer, float decay, float growth)
 {
-    /*
-        Similar to the solo version, but the values range from -1.0 to 1.0, instead of 0.0 to 1.0
+	/*
+		Similar to the solo version, but the values range from -1.0 to 1.0, instead of 0.0 to 1.0
 
-        For every cell in the given layer:
+		For every cell in the given layer:
 
-        1) Get the value of each neighbor and apply decay factor
-        2) Keep the highest ABSOLUTE value from step 1
-        3) Linearly interpolate from the cell's current value to the value from step 2
-           with the growing factor as a coefficient.  Make use of the lerp helper function.
-        4) Store the value from step 3 in a temporary layer.
-           A float[40][40] will suffice, no need to dynamically allocate or make a new MapLayer.
+		1) Get the value of each neighbor and apply decay factor
+		2) Keep the highest ABSOLUTE value from step 1
+		3) Linearly interpolate from the cell's current value to the value from step 2
+		   with the growing factor as a coefficient.  Make use of the lerp helper function.
+		4) Store the value from step 3 in a temporary layer.
+		   A float[40][40] will suffice, no need to dynamically allocate or make a new MapLayer.
 
-        After every cell has been processed into the temporary layer, write the temporary layer into
-        the given layer;
-    */
+		After every cell has been processed into the temporary layer, write the temporary layer into
+		the given layer;
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void normalize_solo_occupancy(MapLayer<float> &layer)
+void normalize_solo_occupancy(MapLayer<float>& layer)
 {
-    /*
-        Determine the maximum value in the given layer, and then divide the value
-        for every cell in the layer by that amount.  This will keep the values in the
-        range of [0, 1].  Negative values should be left unmodified.
-    */
+	/*
+		Determine the maximum value in the given layer, and then divide the value
+		for every cell in the layer by that amount.  This will keep the values in the
+		range of [0, 1].  Negative values should be left unmodified.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void normalize_dual_occupancy(MapLayer<float> &layer)
+void normalize_dual_occupancy(MapLayer<float>& layer)
 {
-    /*
-        Similar to the solo version, but you need to track greatest positive value AND 
-        the least (furthest from 0) negative value.
+	/*
+		Similar to the solo version, but you need to track greatest positive value AND
+		the least (furthest from 0) negative value.
 
-        For every cell in the given layer, if the value is currently positive divide it by the
-        greatest positive value, or if the value is negative divide it by -1.0 * the least negative value
-        (so that it remains a negative number).  This will keep the values in the range of [-1, 1].
-    */
+		For every cell in the given layer, if the value is currently positive divide it by the
+		greatest positive value, or if the value is negative divide it by -1.0 * the least negative value
+		(so that it remains a negative number).  This will keep the values in the range of [-1, 1].
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-void enemy_field_of_view(MapLayer<float> &layer, float fovAngle, float closeDistance, float occupancyValue, AStarAgent *enemy)
+void enemy_field_of_view(MapLayer<float>& layer, float fovAngle, float closeDistance, float occupancyValue, AStarAgent* enemy)
 {
-    /*
-        First, clear out the old values in the map layer by setting any negative value to 0.
-        Then, for every cell in the layer that is within the field of view cone, from the
-        enemy agent, mark it with the occupancy value.  Take the dot product between the view
-        vector and the vector from the agent to the cell, both normalized, and compare the
-        cosines directly instead of taking the arccosine to avoid introducing floating-point
-        inaccuracy (larger cosine means smaller angle).
+	/*
+		First, clear out the old values in the map layer by setting any negative value to 0.
+		Then, for every cell in the layer that is within the field of view cone, from the
+		enemy agent, mark it with the occupancy value.  Take the dot product between the view
+		vector and the vector from the agent to the cell, both normalized, and compare the
+		cosines directly instead of taking the arccosine to avoid introducing floating-point
+		inaccuracy (larger cosine means smaller angle).
 
-        If the tile is close enough to the enemy (less than closeDistance),
-        you only check if it's visible to enemy.  Make use of the is_clear_path
-        helper function.  Otherwise, you must consider the direction the enemy is facing too.
-        This creates a radius around the enemy that the player can be detected within, as well
-        as a fov cone.
-    */
+		If the tile is close enough to the enemy (less than closeDistance),
+		you only check if it's visible to enemy.  Make use of the is_clear_path
+		helper function.  Otherwise, you must consider the direction the enemy is facing too.
+		This creates a radius around the enemy that the player can be detected within, as well
+		as a fov cone.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 }
 
-bool enemy_find_player(MapLayer<float> &layer, AStarAgent *enemy, Agent *player)
+bool enemy_find_player(MapLayer<float>& layer, AStarAgent* enemy, Agent* player)
 {
-    /*
-        Check if the player's current tile has a negative value, ie in the fov cone
-        or within a detection radius.
-    */
+	/*
+		Check if the player's current tile has a negative value, ie in the fov cone
+		or within a detection radius.
+	*/
 
-    const auto &playerWorldPos = player->get_position();
+	const auto& playerWorldPos = player->get_position();
 
-    const auto playerGridPos = terrain->get_grid_position(playerWorldPos);
+	const auto playerGridPos = terrain->get_grid_position(playerWorldPos);
 
-    // verify a valid position was returned
-    if (terrain->is_valid_grid_position(playerGridPos) == true)
-    {
-        if (layer.get_value(playerGridPos) < 0.0f)
-        {
-            return true;
-        }
-    }
+	// verify a valid position was returned
+	if (terrain->is_valid_grid_position(playerGridPos) == true)
+	{
+		if (layer.get_value(playerGridPos) < 0.0f)
+		{
+			return true;
+		}
+	}
 
-    // player isn't in the detection radius or fov cone, OR somehow off the map
-    return false;
+	// player isn't in the detection radius or fov cone, OR somehow off the map
+	return false;
 }
 
-bool enemy_seek_player(MapLayer<float> &layer, AStarAgent *enemy)
+bool enemy_seek_player(MapLayer<float>& layer, AStarAgent* enemy)
 {
-    /*
-        Attempt to find a cell with the highest nonzero value (normalization may
-        not produce exactly 1.0 due to floating point error), and then set it as
-        the new target, using enemy->path_to.
+	/*
+		Attempt to find a cell with the highest nonzero value (normalization may
+		not produce exactly 1.0 due to floating point error), and then set it as
+		the new target, using enemy->path_to.
 
-        If there are multiple cells with the same highest value, then pick the
-        cell closest to the enemy.
+		If there are multiple cells with the same highest value, then pick the
+		cell closest to the enemy.
 
-        Return whether a target cell was found.
-    */
+		Return whether a target cell was found.
+	*/
 
-    // WRITE YOUR CODE HERE
+	// WRITE YOUR CODE HERE
 
-    return false; // REPLACE THIS
+	return false; // REPLACE THIS
 }
